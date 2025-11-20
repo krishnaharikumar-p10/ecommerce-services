0d6d7fbc415394e84d5fa55dabb060983656403a
KrishnaHHK <krishnahari2k19@gmail.com>
2025-11-21 01:12:13 +0530

    Updated code

---
+++ order-service/order-service/src/main/java/com/tech/order_service/dto/OrderEventMessage.java
@@ -0,0 +1,42 @@
+package com.tech.order_service.dto;
+
+class OrderEventMessage {
+    private String orderNumber;
+    private String eventId;
+    private String eventType; 
+    private OrderDTO orderDto;
+    
+   public OrderEventMessage() {}
+    
+   public OrderEventMessage(String orderNumber, String eventId, String eventType, OrderDTO orderDto) {
+		super();
+		this.orderNumber = orderNumber;
+		this.eventId = eventId;
+		this.eventType = eventType;
+		this.orderDto = orderDto;
+	}
+	public String getOrderNumber() {
+		return orderNumber;
+	}
+	public void setOrderNumber(String orderNumber) {
+		this.orderNumber = orderNumber;
+	}
+	public String getEventId() {
+		return eventId;
+	}
+	public void setEventId(String eventId) {
+		this.eventId = eventId;
+	}
+	public String getEventType() {
+		return eventType;
+	}
+	public void setEventType(String eventType) {
+		this.eventType = eventType;
+	}
+	public OrderDTO getOrderDto() {
+		return orderDto;
+	}
+	public void setOrderDto(OrderDTO orderDto) {
+		this.orderDto = orderDto;
+	}
+}
---
+++ order-service/order-service/src/main/java/com/tech/order_service/exceptions/GlobalExceptionalHandler.java
@@ -0,0 +1,15 @@
+package com.tech.order_service.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionalHandler {
+	
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        return new ResponseEntity<>("Something went wrong: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}

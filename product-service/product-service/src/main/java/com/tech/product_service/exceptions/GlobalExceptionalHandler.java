6c704b527321a99f7cc7a6cae812c3250f4fdaba
KrishnaHHK <krishnahari2k19@gmail.com>
2025-11-28 16:02:08 +0530

    updated

--- product-service/product-service/src/main/java/com/tech/product_service/exceptions/GlobalExceptionalHandler.java
+++ product-service/product-service/src/main/java/com/tech/product_service/exceptions/GlobalExceptionalHandler.java
@@ -5,10 +5,13 @@ import java.net.URI;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
+
+import jakarta.servlet.http.HttpServletRequest;
 
 @ControllerAdvice
 public class GlobalExceptionalHandler {
@@ -20,6 +23,20 @@ public class GlobalExceptionalHandler {
 	    problem.setDetail(ex.getMessage());
 	    problem.setInstance(URI.create(((ServletWebRequest) request).getRequest().getRequestURI()));
 	    return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(problem);
+	}
+	
+	@ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+	public ResponseEntity<ProblemDetail> handleMethodNotSupported(
+	        HttpRequestMethodNotSupportedException ex, WebRequest request) {
+
+	    HttpServletRequest servletRequest = ((ServletWebRequest) request).getRequest();
+
+	    ProblemDetail problem = ProblemDetail.forStatus(HttpStatus.METHOD_NOT_ALLOWED);
+	    problem.setTitle("Method Not Allowed");
+	    problem.setDetail(ex.getMessage());
+	    problem.setInstance(java.net.URI.create(servletRequest.getRequestURI()));
+
+	    return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED).body(problem);
 	}
 	
 	@ExceptionHandler(ProductNotFoundException.class)
